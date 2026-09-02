0eceb08e508a066f7c121eac8b02e96f238797a2
Manthan-Moondra-26 <manthanmoondra0@gmail.com>
2026-09-02 14:15:29 +0530

    Implement firstMissingPositive method in Solution class

---
+++ Arrays/first_missing_positive.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            seen.add(num);
+        }
+        for (int i = 1; i <= nums.length + 1; i++) {
+            if (!seen.contains(i)) {
+                return i;
+            }
+        }
+        return nums.length + 1;
+    }
+}
